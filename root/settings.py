7e2913ff853d04e00dfe018ef2e5f1d2bf461a83
jatinsinghnp <js8971105@gmail.com>
2022-12-14 12:14:32 +0545

    all done

--- root/settings.py
+++ root/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
+# SECRET_KEY = "HELLOPERIzinwheere2340i3-45)(*)(*)(&()()dfjbgf4()&)()"
 
 SECRET_KEY = env("SECRET_KEY")
 
